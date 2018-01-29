556719f3e09e3405e8b4d6dfd90b3dcac7d5ad7b
Aaron <ledzeppelindrummer@gmail.com>
2018-01-28 19:17:50 -0500

    Changed the FAB icon to add icon

--- app/src/main/java/edu/ycp/cs482/iorc/CharacterListActivity.java
+++ app/src/main/java/edu/ycp/cs482/iorc/CharacterListActivity.java
@@ -48,7 +48,7 @@ public class CharacterListActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Create new Character Action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
