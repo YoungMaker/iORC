8544c9629776afd42ec4cedd1146e0f169de38a6
Aaron <ledzeppelindrummer@gmail.com>
2018-01-31 12:19:30 -0500

    Added a bunch of blank activities to the project. Later, these will be strung together and hooked up to data.

--- app/src/main/java/edu/ycp/cs482/iorc/CharacterListActivity.java
+++ app/src/main/java/edu/ycp/cs482/iorc/CharacterListActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.design.widget.FloatingActionButton;
@@ -68,6 +69,10 @@ public class CharacterListActivity extends AppCompatActivity {
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
         recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(this, DummyContent.ITEMS, mTwoPane));
+        DividerItemDecoration itemDecor = new DividerItemDecoration(recyclerView.getContext(),
+                DividerItemDecoration.VERTICAL); //this should probably get the layoutManager's preference.
+        recyclerView.addItemDecoration(itemDecor);
+
     }
 
     public static class SimpleItemRecyclerViewAdapter
