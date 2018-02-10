880cb12607d7f36f23716a6a557903fcf85c9b6d
Hunter WIllingham <hwilling@ycp.edu>
2018-02-10 12:30:58 -0500

    fixed classrace detail title showing description instead of name

--- app/src/main/java/edu/ycp/cs482/iorc/ClassRaceDetailFragment.java
+++ app/src/main/java/edu/ycp/cs482/iorc/ClassRaceDetailFragment.java
@@ -49,7 +49,7 @@ public class ClassRaceDetailFragment extends Fragment {
             Activity activity = this.getActivity();
             CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
             if (appBarLayout != null) {
-                appBarLayout.setTitle(mItem.content);
+                appBarLayout.setTitle(mItem.name);
             }
         }
     }
