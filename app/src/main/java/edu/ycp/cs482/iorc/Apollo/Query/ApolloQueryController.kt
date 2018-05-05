41e9081669b84c49be6f61103569b6c6fb4d8341
Hunter Willingham <hwilling@ycp.edu>
2018-05-05 18:29:25 -0400

    issue #142 iQueryController added added apolloiquerycontroller using iqueryController as an interface

---
+++ app/src/main/java/edu/ycp/cs482/iorc/Apollo/Query/ApolloQueryController.kt
@@ -0,0 +1,11 @@
+package edu.ycp.cs482.iorc.Apollo.Query
+
+class ApolloQueryCntroller:IQueryController {
+    override fun loginQuery(email: String, password: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun userCharactersQuery(userID: String): QueryData {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
--- app/src/main/java/edu/ycp/cs482/iorc/Apollo/Query/IQueryController.kt
+++ app/src/main/java/edu/ycp/cs482/iorc/Apollo/Query/IQueryController.kt
@@ -2,4 +2,5 @@ package edu.ycp.cs482.iorc.Apollo.Query
 
 interface IQueryController {
     fun loginQuery(email:String, password:String)
+    fun userCharactersQuery(userID:String):QueryData
 }
